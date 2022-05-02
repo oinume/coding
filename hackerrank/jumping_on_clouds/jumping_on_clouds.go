84b4470e99d2c62ff4cb376c75c82a83a7671b52
Kazuhiro Oinuma <oinume@gmail.com>
2022-05-02 20:01:26 +0900

    Implement jumpingOnClouds

--- hackerrank/jumping_on_clouds/jumping_on_clouds.go
+++ hackerrank/jumping_on_clouds/jumping_on_clouds.go
@@ -18,9 +18,19 @@ import (
  */
 
 func jumpingOnClouds(c []int32) int32 {
-	// Write your code here
-	// 1 or 2 jump
-	return 0
+	count := 0
+	for i := 0; i < len(c); {
+		if i+2 < len(c) && c[i+2] == 0 {
+			i += 2
+			count++
+		} else if i+1 < len(c) && c[i+1] == 0 {
+			i += 1
+			count++
+		} else {
+			i += 1
+		}
+	}
+	return int32(count)
 }
 
 func main() {
