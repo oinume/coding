031a1114d16a82debeea5c77575ae444436d49d1
Kazuhiro Oinuma <oinume@gmail.com>
2022-05-11 11:11:07 +0900

    Add unit test for rotateLeft

---
+++ hackerrank/rotate_left_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_rotateLeft(t *testing.T) {
+	tests := map[string]struct {
+		d     int32
+		array []int32
+		want  []int32
+	}{
+		"example0": {
+			d:     2,
+			array: []int32{1, 2, 3, 4, 5},
+			want:  []int32{3, 4, 5, 1, 2},
+		},
+		"example1": {
+			d:     4,
+			array: []int32{1, 2, 3, 4, 5},
+			want:  []int32{5, 1, 2, 3, 4},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := rotateLeft(test.d, test.array)
+			if !reflect.DeepEqual(test.want, got) {
+				t.Errorf("[%s] want=%+v, got=%+v", name, test.want, got)
+			}
+		})
+	}
+}
