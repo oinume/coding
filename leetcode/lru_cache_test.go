20fd3b60ed4693aa059adfdc28b5057cdd8ab67e
Kazuhiro Oinuma <oinume@gmail.com>
2023-07-17 17:37:50 +0900

    Add LRU cache

--- leetcode/lru_cache_test.go
+++ leetcode/lru_cache_test.go
@@ -1,4 +1,4 @@
-package lru_cache
+package leetcode
 
 import (
 	"fmt"
@@ -6,6 +6,7 @@ import (
 	"math"
 	"strings"
 	"sync"
+	"testing"
 )
 
 /*
@@ -46,7 +47,7 @@ type entry struct {
 	age   int
 }
 
-func Constructor(capacity int) LRUCache {
+func NewLRUCache(capacity int) LRUCache {
 	if capacity < 0 {
 		panic("can't specify negative capacity.")
 	}
@@ -116,4 +117,68 @@ func (c *LRUCache) Dump(w io.Writer) error {
 		return err
 	}
 	return nil
+}
+
+//LRUCache cache = new LRUCache( 2 /* capacity */ );
+//
+//cache.put(1, 1);
+//cache.put(2, 2);
+//cache.get(1);       // returns 1
+//cache.put(3, 3);    // evicts key 2
+//cache.get(2);       // returns -1 (not found)
+//cache.put(4, 4);    // evicts key 1
+//cache.get(1);       // returns -1 (not found)
+//cache.get(3);       // returns 3
+//cache.get(4);       // returns 4
+
+func TestLRUCache_PutAndGet(t *testing.T) {
+	cache := NewLRUCache(2)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+
+	if got, want := cache.Get(1), 1; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+
+	cache.Put(3, 3) // evicts key 2
+	if got, want := cache.Get(2), -1; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+
+	cache.Put(4, 4) // evicts key 1
+	if got, want := cache.Get(1), -1; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+
+	if got, want := cache.Get(3), 3; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+
+	if got, want := cache.Get(4), 4; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+}
+
+func TestLRUCache_PutAndGet2(t *testing.T) {
+	/*
+		NewLRUCache(2)
+		put(2, 1)
+		put(2, 2)
+		get(2)
+		put(1, 1)
+		put(4, 1)
+		get(2)
+	*/
+	cache := NewLRUCache(2)
+	cache.Put(2, 1)
+	cache.Put(2, 2)
+	if got, want := cache.Get(2), 2; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+
+	cache.Put(1, 1)
+	cache.Put(4, 1)
+	if got, want := cache.Get(2), -1; got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
 }
